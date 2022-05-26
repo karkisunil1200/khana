c71447bce6af1f2ddf82e843eeaaa4bcb2f81f96
skarki1200 <karkisunil1200@gmail.com>
2022-05-25 19:10:05 -0500

    Added

---
+++ screens/CategoriesScreen.js
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import React from "react";
+import { CATEGORIES } from "../data/dummy_data";
+import CategoryGridTile from "../components/CategoryGridTile";
+
+function renderCategoryItem(itemData) {
+  return (
+    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+  );
+}
+const CategoriesScreen = () => {
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+      numColumns={2}
+    />
+  );
+};
+
+export default CategoriesScreen;
+
+const styles = StyleSheet.create({});
